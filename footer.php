a3f82fe1475771a74ea002859057f769775208c3
Tamilarasan.S <94790450+ts1844@users.noreply.github.com>
2023-06-19 10:21:48 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  Blood Bank & Donation Management
+  <br>
+  </center>
+  </div>
+
+
+</body>
+
+</html>
